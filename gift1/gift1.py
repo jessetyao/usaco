6c13d1bfb7f40cd7a846aa89a9e7173c27f79143
Jesse Yao <jessetyao@gmail.com>
2022-02-02 22:04:33 -0800

    gift1

--- gift1/gift1.py
+++ gift1/gift1.py
@@ -1,18 +1,30 @@
-content = []
+"""
+ID: jessety1
+LANG: PYTHON3
+TASK: gift1
+"""
 
-with open("gift1.in") as f:
-    content = f.readlines()
-    
+people = {}
+po = []
+with open('gift1.in','r') as fin:
+    np = int(fin.readline().strip())
+    for i in range(np):
+        name= fin.readline().strip()
+        people[name] = 0
+        po.append(name)
+        
+    for i in range(np):
+        #name
+        giver1 = fin.readline().strip()
+        #amount give and number of people
+        (money1, ptg) = [int(x) for x in fin.readline().strip().split()]
+        if ptg != 0:
+            people[giver1] = people[giver1] -money1 + money1 % ptg
 
-content = [x.strip(" \n\r") for x in content] 
-print(content)
-num_of_people = int(content[0])
-
-peoples = []
-
-for i in range(1, 1+num_of_people):
-    people = {"name": content[i]}
-    peoples.append(people)
-
-print(peoples)
-
+        for j in range(ptg):
+            #GET list of people to give to
+            g = fin.readline().strip()
+            people[g] += money1//ptg
+with open ('gift1.out','w') as fout:
+    for p in po:    
+        fout.write (f"{p} {people[p]}\n")
